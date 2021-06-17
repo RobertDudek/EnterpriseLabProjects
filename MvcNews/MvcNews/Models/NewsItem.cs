a9fc60e71b0f1ea3c377f314ad4a49776ba57614
RobertDudek <robert.dudek@student.put.poznan.pl>
2021-06-17 19:52:38 +0200

    MvcNews Lab

---
+++ MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcNews.Models
+{
+    public class NewsItem
+    {
+        public int Id
+        { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime TimeStamp
+        { get; set; }
+        [Required(ErrorMessage = "The message is required"), StringLength(140, MinimumLength = 5, ErrorMessage = "Message must be between 5 and 140 characters.")]
+        public string Text
+        { get; set; }
+        [Timestamp]
+        public byte[] RowVersion
+        { get; set; }
+    }
+}
